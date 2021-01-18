b7b490e431dd3d3ca912603d2d9b0009e65ce1c3
Evgen <yujinwork19@gmail.com>
2021-01-18 11:16:24 +0200

    fix slider horizontal scroll, delete onclick from html, use mnemonic for copyright

--- main.js
+++ main.js
@@ -29,21 +29,6 @@ let slideIndex = 1;
 /* Вызываем функцию, которая реализована ниже: */
 showSlides(slideIndex);
 
-/* Увеличиваем индекс на 1 — показываем следующий слайд: */
-function nextSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
-function previousSlide() {
-    showSlides(slideIndex -= 1);
-}
-
-/* Устанавливаем текущий слайд: */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
     /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
     let slides = document.getElementsByClassName("slider__item");
@@ -69,4 +54,31 @@ function showSlides(n) {
 
     dots[slideIndex - 1].classList.add("active")
 }
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+let nextBtn = document.querySelector('.next')
+
+nextBtn.addEventListener("click", function () {
+    showSlides(slideIndex += 1);
+})
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+let previousBtn = document.querySelector('.previous')
+
+previousBtn.addEventListener("click", function () {
+    showSlides(slideIndex -= 1);
+})
+
+/* Устанавливаем текущий слайд: */
+let dots = document.querySelectorAll(".dots")
+
+dots.forEach((item,index)=> {
+    item.addEventListener("click",function (){
+        showSlides(slideIndex = index+1);
+    })
+})
+
+
+
+
 
